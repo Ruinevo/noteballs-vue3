135ef91b9c9fc1aa6f52cb0932d901bcee9cea0e
Rinat Sadykov <sadykovra@mail.ru>
2023-06-03 10:54:21 +0500

    init project

---
+++ src/App.vue
@@ -0,0 +1,24 @@
+
+
+<template>
+  <header>
+
+    <div class="wrapper">
+
+
+      <nav class="">
+        <router-link to="/">Home</router-link>
+        <router-link to="/about">About</router-link>
+      </nav>
+    </div>
+  </header>
+
+  <router-view />
+</template>
+
+<script setup>
+import {RouterLink, RouterView} from 'vue-router'</script>
+
+<style lang="sass" scoped>
+
+</style>
---
+++ src/views/home-view.vue
@@ -0,0 +1,36 @@
+<template>
+  <main class="p-10" ref="test">
+    <button @click="addPropertyToObject" class="p-4 rounded-3xl bg-gray-700 mr-2 text-white">+</button>
+    <div>{{ state.a }}</div>
+    <button @click="updateString" class="p-4 rounded-3xl bg-gray-700 text-white">-</button>
+  </main>
+</template>
+
+
+<script setup>
+
+import {onMounted, ref} from "vue";
+
+let state = ref({ a: 10 })
+
+
+const test = ref(null)
+
+const addPropertyToObject = () => {
+  state.value = { a: 100 }
+}
+
+const updateString = () => {
+  state.value.b = 30
+}
+
+
+onMounted(() => {
+  console.log(test.value)
+})
+
+
+
+</script>
+
+
